c3816415ed30985b06da57bac37ed735d5cfdc5e
sanjwilli <williamsanjay77@gmail.com>
2017-05-29 13:03:03 -0500

    added about not complete

--- assets/js/Controllers/mainController.js
+++ assets/js/Controllers/mainController.js
@@ -1,4 +1,5 @@
 app.controller('MainController', function($scope){
 	$scope.name = "Sanjay Williams";
-	$scope.copyright = "All content copyright 2017"
+	$scope.copyright = "All content copyright 2017";
+	$scope.email = "williamsanjay77@gmail.com";
 })
